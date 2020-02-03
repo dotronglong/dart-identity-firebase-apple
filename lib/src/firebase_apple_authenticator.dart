51b81f9d9a1ea547adf56de0532406a59dbb8d58
Lucas <me@dotronglong.com>
2020-02-04 00:09:22 +0800

    Finish implementation

--- lib/src/firebase_apple_authenticator.dart
+++ lib/src/firebase_apple_authenticator.dart
@@ -1,19 +1,55 @@
+import 'dart:io';
+
+import 'package:apple_sign_in/apple_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:identity/identity.dart';
 import 'package:sso/sso.dart';
 
-class FirebaseAppleAuthenticator implements Authenticator {
+class FirebaseAppleAuthenticator
+    with WillNotify, WillConvertUser
+    implements Authenticator {
+  final bool _isSupport = Platform.isIOS;
 
   @override
-  WidgetBuilder get action => (context) => ActionButton(
-      onPressed: () => authenticate(context),
-      color: Color.fromRGBO(66, 103, 178, 1),
-      textColor: Colors.white,
-      icon: Image.asset("images/apple.png",
-          package: "identity_firebase_apple", width: 24, height: 24),
-      text: "Sign In With Apple");
+  WidgetBuilder get action => (context) => _isSupport
+      ? ActionButton(
+          onPressed: () => authenticate(context),
+          color: Colors.black,
+          textColor: Colors.white,
+          icon: Image.asset("images/apple.png",
+              package: "identity_firebase_apple", width: 24, height: 24),
+          text: "Sign In With Apple")
+      : Container();
 
   @override
-  Future<Function> authenticate(BuildContext context, [Map parameters]) {
+  Future<void> authenticate(BuildContext context, [Map parameters]) async {
+    final AuthorizationResult result = await AppleSignIn.performRequests([
+      AppleIdRequest(requestedScopes: [Scope.email, Scope.fullName])
+    ]);
+    switch (result.status) {
+      case AuthorizationStatus.authorized:
+        notify(context, "Processing ...");
+        final String idToken =
+            String.fromCharCodes(result.credential.identityToken);
+        final String accessToken =
+            String.fromCharCodes(result.credential.authorizationCode);
+        final OAuthProvider oAuthProvider =
+            OAuthProvider(providerId: "apple.com");
+        final AuthCredential credential = oAuthProvider.getCredential(
+            idToken: idToken, accessToken: accessToken);
 
+        return FirebaseAuth.instance
+            .signInWithCredential(credential)
+            .then((result) => convert(result.user))
+            .then((user) => Identity.of(context).user = user)
+            .catchError(Identity.of(context).error);
+      case AuthorizationStatus.cancelled:
+        Identity.of(context).error("User cancelled!");
+        break;
+      case AuthorizationStatus.error:
+        Identity.of(context).error("Unable to sign in!");
+        break;
+    }
   }
 }
